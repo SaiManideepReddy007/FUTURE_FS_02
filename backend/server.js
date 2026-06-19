ec038d5ded56ab1a9f6961acf8875ae2e94162e3
Sai Manideep Reddy Gujjula <gujjulasaimanideepreddy@gmail.com>
2026-06-19 16:30:53 +0530

    NestCRM - Complete Client Lead Management System

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://future-fs-02-iota-three.vercel.app/']
+}))
 app.use(express.json());
 
 app.use('/api/leads', require('./routes/leadRoutes'));
